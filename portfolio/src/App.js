017e25b62fd1a6f07faa706266d571342bd2b9a7
Badr Almadhi <badr.almadhi95@gmail.com>
2024-03-09 21:15:18 -0700

    finished home

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Nav from "./components/NavTabs";
 
 export default function App () {
     return(
-        <PortfolioContainer/>
+        < PortfolioContainer />
     );
 }
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,34 +1,45 @@
 import React from "react";
-import NavTabs from "../NavTabs";
+import "../styles.css/Home.css";
+import image from "../images/myphoto.png";
+import Certificate from "../images/certificate.png";
 
-export default function Home () {
-    return (
-        <section className="home">
-        <div className="home-content">
-          <h1>Hi, I'm Badr Almadhi</h1>
-          <h3>Coding BootCamp Student</h3>
-          <p>"Hello! I'm Badr Almadhi, a dedicated coding enthusiast currently who have graduated from a <a href="images/certificate.png"> coding bootcamp</a> in 2023.
-            My journey into the world of programming began with a burning curiosity and a desire to bring my
-            creative ideas to life through technology. With each line of code I write, I'm building a bridge
-            between imagination and reality. I'm passionate about learning and am constantly seeking new
-            challenges to expand my skills. Beyond coding, I enjoy problem-solving, collaborating with
-            fellow learners, and embracing the ever-evolving tech landscape. I'm excited to see where this
-            coding adventure takes me and am committed to growing as a developer with each step of the way."</p>
-            <div class="btn-box">
-              <a href="images/resumePhoto.png">Hire Me</a>
-              <a href="contact/contact.html">Let's Talk</a>
-            </div>
-            {/* <img class='imeg' src="images/myphoto.png"> */}
+export default function Home() {
+  const imageUrl = image;
+  const certificate = Certificate
+  return (
+    <section className="home">
+      <div className="home-content">
+        <h1>Hi, I'm Badr Almadhi</h1>
+        <h3>Coding BootCamp Student</h3>
+        <p>
+          "Hello! I'm Badr Almadhi, a dedicated coding enthusiast currently who
+          have graduated from a{" "}
+          <a href="images/certificate.png"> coding bootcamp</a> in 2023. My
+          journey into the world of programming began with a burning curiosity
+          and a desire to bring my creative ideas to life through technology.
+          With each line of code I write, I'm building a bridge between
+          imagination and reality. I'm passionate about learning and am
+          constantly seeking new challenges to expand my skills. Beyond coding,
+          I enjoy problem-solving, collaborating with fellow learners, and
+          embracing the ever-evolving tech landscape. I'm excited to see where
+          this coding adventure takes me and am committed to growing as a
+          developer with each step of the way."
+        </p>
+        <div className="btn-box">
+          <a href="images/resumePhoto.png">Resume</a>
+          <a href="contact/contact.html">Let's Talk!</a>
         </div>
-    
-        <div className="home-sci">
-          <a href="https://github.com/BadrAlmadhi"><i className='bx bxl-github'></i></a>
-          <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"><i class='bx bxl-linkedin'></i></a>
-          
-        </div>
-    
-       
-      </section>
-    
-    );
+         <img src={imageUrl} alt="My photo"/>
+      </div>
+
+      <div className="home-sci">
+        <a href="https://github.com/BadrAlmadhi">
+          <i className="bx bxl-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile">
+          <i className="bx bxl-linkedin"></i>
+        </a>
+      </div>
+    </section>
+  );
 }
