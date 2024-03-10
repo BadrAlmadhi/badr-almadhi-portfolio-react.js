9d302064480a4e45936082ad352b9dbbed9ba5a1
Badr Almadhi <badr.almadhi95@gmail.com>
2024-03-10 14:12:14 -0600

    deleted folder I will re-do due to folder structor problem

--- portfolio/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-import Nav from "./components/NavTabs";
-
-export default function App () {
-    return(
-        < PortfolioContainer />
-    );
-}
