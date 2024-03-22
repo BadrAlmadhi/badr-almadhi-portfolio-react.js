42267b95b143b8bb3c03605ae78e7056e9f28a77
Badr Almadhi <badr.almadhi95@gmail.com>
2024-03-21 23:29:41 -0600

    added componenets with tilwindCSS

---
+++ portfolio/src/data.js
@@ -0,0 +1,45 @@
+import node from '../src/images/node.png'
+export const projects = [
+  {
+    title: "Readme file generator",
+    subtitle: "JavaScript and Node.js",
+    description:
+      "This readme file generator is build to help users make their life easer by setting up the main component of the readme file and then they can add to it",
+    image: node,
+    link: "https://github.com/BadrAlmadhi/readme_file_generator",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "add link",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "add link"
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "add link"
+  },
+];
+
+export const skills = [
+    "JavaScript",
+    "React",
+    "Node.js",
+  ];
+
+  export const testimonials = [
+    // testimonials data...
+  ];
